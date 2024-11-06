a753592d66ef912d6724c20275c1ba8ae8b05687
Sherman-Firefly <efracw2007@gmail.com>
2024-11-06 17:17:26 +0800

    Set Operations

---
+++ Sets Operations/Set Operations.py
@@ -0,0 +1,18 @@
+#sets
+myset={}
+print(myset)
+myset={1,2,3,4,5,6,7}
+print(myset)
+
+#mix
+myset={"Coding", 2, 4.4, (1,4,5)}
+print(myset)
+
+nextset={1,2,2,3,4,5,5,6,6,7,8,9,20,20,11,10}
+print(nextset)
+
+nextset=set([1,2,3,4,5])
+print(nextset,"\n")
+
+nextset.pop()
+print(nextset)
