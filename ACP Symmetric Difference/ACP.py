29557bbb9da027aef5e2d7df62c3119ddb99128a
Sherman-Firefly <efracw2007@gmail.com>
2024-11-07 14:27:23 +0800

    ACP

---
+++ ACP Symmetric Difference/ACP.py
@@ -0,0 +1,10 @@
+#using ACP instructions for values
+set1={"blue", "green"}
+set2={"blue", "yellow"}
+d=set1.symmetric_difference(set2)
+print(d)
+
+set3={1,2,3,4,5}
+set4={1,5,6,7,8,9}
+e=set3.symmetric_difference(set4)
+print(e)
